3ac7816102ee17b5f836f99d93560a186265c12e
Peter Leth <petergl@protonmail.com>
2017-10-25 23:09:28 +0200

    Renamed ping.js to datbot.js

--- datbot.js
+++ datbot.js
@@ -24,57 +24,63 @@ client.on('ready', () => {
 client.on('message', message => {
     // Switch between different cases
     switch(message.content) {
+        
         case '!ping':
             // Pong the pinger!
             message.channel.send('pong');
             break; // Remember to break!
+
         case '!help':
             // Send a pm with a table of useful commands 
             // (find out how to format a table of useful commands)
             message.channel.send('Sent help!')
             message.author.sendMessage('Hope I\'m helping!');
             break;
+
         case '!code': // Nesting cases creates synonyms!
         case '!contribute':
         case '!source':
             // Send a link to the source code
             message.channel.send('Here ye go, ' + message.author + ' - ' + link);
             break;
+
         case '!ermin':
+            // Quote the man himself
             const ermin = client.emojis.find("name", "incest");
+
             switch(Math.ceil(Math.random()*6)) {
               case 1: 
                 message.channel.send(`${ermin} Drenge, skal vi ikke spille noget minecraft? ${ermin}`);
-                break; // Remember to break!
+                break;
               case 2: 
                  message.channel.send(`${ermin} Pokémon, jeg vælger dig! ${ermin}`);
-                 break; // Remember to break!
+                 break;
               case 3: 
                  message.channel.send(`${ermin} Jeg har lyst til at X med min søsters Y ${ermin}`);
-                 break; // Remember to break!
+                 break;
               case 4: 
                  message.channel.send(`${ermin} Ej, Calculus Beta. Det er er bare det fedeste fag NOGENSINDE. ${ermin}`);
-                 break; // Remember to break!
+                 break;
               case 5: 
                  message.channel.send(`${ermin} Hvad? ${ermin}`);
-                 break; // Remember to break!
+                 break;
               case 6: 
                  message.channel.send(`${ermin} Klokken er 3 om natten... Min pik er stiv... Sover min søster? ${ermin}`);
-                 break; // Remember to break!
+                 break;
               case 7: 
                  message.channel.send(`${ermin} <blank5> ${ermin}`);
-                 break; // Remember to break!
+                 break;
               case 8: 
                  message.channel.send(`${ermin} <blank6> ${ermin}`);
-                 break; // Remember to break!
+                 break;
               case 9: 
                  message.channel.send(`${ermin} <blank7> ${ermin}`);
-                 break; // Remember to break!
+                 break;
               case 10: 
                  message.channel.send(`${ermin} <blank8> ${ermin}`);
-                 break; // Remember to break!
-             }
-            break; // Remember to break!
+                 break;
+            }
+            break;
     }
 });
 
